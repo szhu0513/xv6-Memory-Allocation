9b3bfa827200de1e2df17419b56d10edd92e7179
Shiyu Zhu <48394203+szhu0513@users.noreply.github.com>
2019-11-25 16:32:06 -0600

    Add files via upload

---
+++ v1a/kalloc.c
@@ -0,0 +1,131 @@
+// Physical memory allocator, intended to allocate
+// memory for user processes, kernel stacks, page table pages,
+// and pipe buffers. Allocates 4096-byte pages.
+
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "spinlock.h"
+
+void freerange(void *vstart, void *vend);
+void freerange2(void *vstart, void *vend);
+extern char end[]; // first address after kernel loaded from ELF file
+                   // defined by the kernel linker script in kernel.ld
+
+struct run {
+  struct run *next;
+};
+
+struct {
+  struct spinlock lock;
+  int use_lock;
+  struct run *freelist;
+  int pf[MAXPG];
+} kmem;
+
+// Initialization happens in two phases.
+// 1. main() calls kinit1() while still using entrypgdir to place just
+// the pages mapped by entrypgdir on free list.
+// 2. main() calls kinit2() with the rest of the physical pages
+// after installing a full page table that maps them on all cores.
+void
+kinit1(void *vstart, void *vend)
+{
+  initlock(&kmem.lock, "kmem");
+  kmem.use_lock = 0;
+  freerange(vstart, vend);
+}
+
+void
+kinit2(void *vstart, void *vend)
+{
+  freerange2(vstart, vend);
+  kmem.use_lock = 1;
+}
+
+void
+freerange(void *vstart, void *vend)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint)vstart);
+  for (int i = 0; i < MAXPG; i++) {
+      kmem.pf[i] = -1;
+  }
+  for(; p + PGSIZE <= (char*)vend; p += PGSIZE) {
+      kfree(p);
+  }
+}
+
+void
+freerange2(void *vstart, void *vend)
+{
+  char *p;
+  p = (char*)PGROUNDUP((uint)vstart);
+  for (int i = 0; i < MAXPG; i++) {
+      kmem.pf[i] = -1;
+  }
+  for(; p + PGSIZE <= (char*)vend; p += PGSIZE) {
+      kfree(p);
+  }
+}
+// Free the page of physical memory pointed at by v,
+// which normally should have been returned by a
+// call to kalloc().  (The exception is when
+// initializing the allocator; see kinit above.)
+void
+kfree(char *v)
+{
+  struct run *r;
+
+  if((uint)v % PGSIZE || v < end || V2P(v) >= PHYSTOP)
+    panic("kfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(v, 1, PGSIZE);
+
+  if(kmem.use_lock)
+    acquire(&kmem.lock);
+  r = (struct run*)v;
+  r->next = kmem.freelist;
+  kmem.freelist = r;
+  if(kmem.use_lock)
+    release(&kmem.lock);
+}
+
+// Allocate one 4096-byte page of physical memory.
+// Returns a pointer that the kernel can use.
+// Returns 0 if the memory cannot be allocated.
+char*
+kalloc(void)
+{
+  struct run *r;
+
+  if(kmem.use_lock)
+    acquire(&kmem.lock);
+  r = kmem.freelist;
+  if(r) {
+      kmem.pf[V2P(r) >> 12] = -2;
+      kmem.freelist = r->next->next;
+  }
+  if(kmem.use_lock)
+    release(&kmem.lock);
+  return (char*)r;
+}
+
+int
+dump_physmem(int* f, int* pids, int numframe) {
+    if (f == 0 || pids == 0) {
+        return -1;
+    }
+    int i = 0;
+    for (int j = MAXPG-1; j > 0 && i < numframe; j--) {
+        if (kmem.pf[j] != -1) {
+            f[i] = j;
+            pids[i] = kmem.pf[j];
+            i++;
+        }
+    }
+    return 0;
+}
